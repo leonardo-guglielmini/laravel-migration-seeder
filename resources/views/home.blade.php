8003819c78f3c14addbba95ae4d923ee973900c3
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2025-03-19 09:33:20 +0100

    Sort trains by departure time and update home view to display train details

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,8 @@
-@dd($trains);
+{{-- @dd($trains); --}}
+
+@foreach ($trains as $train)
+    <div>
+        <h2>{{$train['train_number']}}</h2>
+        <p>{{$train['departure_time']}}</p>
+    </div>
+@endforeach
