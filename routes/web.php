2cc71052c3f954295c3bc228ab3316b6577e07eb
leonardo-guglielmini <leonardo.guglielmini@outlook.it>
2025-03-19 09:29:34 +0100

    Implement train model and controller, update home view to display trains

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Guest\PageController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'index']);
